dbf68fb0bb38ed8f4af75e83ec213c661a2a240f
Lokesh Kumar <109752684+trmxvibs@users.noreply.github.com>
2026-02-19 10:23:07 +0530

    Delete update_date.sh

--- update_date.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# update_date.sh — Updates the timestamp in README.md for the date badge
-
-# Get current timestamp (in seconds)
-timestamp=$(date +%s)
-
-# Update the badge line in README.md
-sed -i "s|https://img.shields.io/date/[0-9]*?label=Last%20Update&color=orange|https://img.shields.io/date/${timestamp}?label=Last%20Update&color=orange|" README.md
